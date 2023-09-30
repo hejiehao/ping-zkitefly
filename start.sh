9dd8c7968ac09e7963010c043852b0941f60dd45
hejiehao <hejiehao12@126.com>
2023-09-30 19:14:26 +0800

    改了一下文件名

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while true; do
-    # 运行 ping-zkitefly.py 脚本
-    python3 ping-zkitefly.py
+    # 运行 main.py 脚本
+    python3 main.py
 
     # 等待一段时间后重新运行脚本
     echo "Script stopped. Restarting in 10 secs..."
